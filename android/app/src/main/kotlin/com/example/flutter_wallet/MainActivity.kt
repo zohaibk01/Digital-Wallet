72906b4af0202ad7be146845029ac1d796f3541e
zohaibk01 <125304164+zohaibk01@users.noreply.github.com>
2024-01-28 20:26:30 +0500

    Add files via upload

---
+++ android/app/src/main/kotlin/com/example/flutter_wallet/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.My Wallet
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,640 @@
+import 'package:flutter/material.dart';
+
+class IconColors {
+  static const Color send = Color(0xffecfaf8);
+  static const Color transfer = Color(0xfffdeef5);
+  static const Color passbook = Color(0xfffff4eb);
+  static const Color more = Color(0xffeff1fe);
+}
+
+class IconImgs {
+  static const String send = "assets/imgs/send.png";
+  static const String transfer = "assets/imgs/transfer.png";
+  static const String passbook = "assets/imgs/passbook.png";
+  static const String more = "assets/imgs/more.png";
+  static const String freeze = "assets/imgs/freeze.png";
+  static const String unlock = "assets/imgs/unlock.png";
+  static const String secret = "assets/imgs/secret.png";
+}
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Digital Wallet',
+      theme: ThemeData(
+        backgroundColor: Color(0xffe7e9f0),
+        fontFamily: 'Raleway',
+      ),
+      home: Wallet(title: "My Wallet"),
+    );
+  }
+}
+
+class MyCards extends StatelessWidget {
+  final String title;
+
+  const MyCards({required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xffdee4eb),
+      appBar: AppBar(
+        title: Text(
+          title,
+          style: TextStyle(
+            color: Colors.black54,
+            fontWeight: FontWeight.bold,
+            fontSize: 21.0,
+          ),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.white,
+        elevation: 0,
+        leading: IconButton(
+          icon: Icon(
+            Icons.menu,
+            color: Colors.black54,
+          ),
+          onPressed: () {},
+        ),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(
+              Icons.add,
+              color: Colors.black54,
+            ),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Container(
+              decoration: BoxDecoration(
+                boxShadow: [
+                  BoxShadow(
+                    blurRadius: 5.0,
+                    color: Colors.grey.shade300,
+                    spreadRadius: 5.0,
+                  ),
+                ],
+                borderRadius: BorderRadius.only(
+                  bottomRight: Radius.circular(51),
+                  bottomLeft: Radius.circular(51),
+                ),
+                color: Colors.white,
+              ),
+              child: GestureDetector(
+                child: CreditCardContainer(),
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
+            ),
+            CustomContainer(
+              child: FittedBox(
+                child: Row(
+                  children: <Widget>[
+                    CustomIconButton(
+                      buttonImg: IconImgs.unlock,
+                      buttonTitle: "UNLOCK PIN/CVV",
+                      circleColor: IconColors.send,
+                      onTap: () {},
+                    ),
+                    CustomIconButton(
+                      buttonImg: IconImgs.freeze,
+                      buttonTitle: "FREEZE CARD",
+                      circleColor: Colors.lime.shade100,
+                      onTap: () {},
+                    ),
+                    CustomIconButton(
+                      buttonImg: IconImgs.secret,
+                      buttonTitle: "SHOW SECRET CODE",
+                      circleColor: Colors.pink.shade100,
+                      onTap: () {},
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Container(
+              child: CustomContainer(
+                child: ListView(
+                  shrinkWrap: true,
+                  children: <Widget>[
+                    Material(
+                      color: Colors.transparent,
+                      child: ListTile(
+                        leading: CircleAvatar(
+                          child: Icon(Icons.atm),
+                          radius: 20,
+                        ),
+                        title: Text(
+                          "ATM CARDLESS",
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                        enabled: true,
+                        onTap: () {},
+                      ),
+                    ),
+                    Material(
+                      color: Colors.transparent,
+                      child: ListTile(
+                        leading: CircleAvatar(
+                          child: Icon(Icons.location_on),
+                          radius: 20,
+                        ),
+                        title: Text(
+                          "ATM LOCATOR",
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                        enabled: true,
+                        onTap: () {},
+                      ),
+                    ),
+                    Material(
+                      color: Colors.transparent,
+                      child: ListTile(
+                        leading: CircleAvatar(
+                          child: Icon(Icons.security),
+                          radius: 20,
+                        ),
+                        title: Text(
+                          "SECURITY CARD",
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                        enabled: true,
+                        onTap: () {},
+                      ),
+                    ),
+                    Material(
+                      color: Colors.transparent,
+                      child: ListTile(
+                        leading: CircleAvatar(
+                          child: Icon(Icons.assessment),
+                          radius: 20,
+                        ),
+                        title: Text(
+                          "LIMITS",
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                        enabled: true,
+                        onTap: () {},
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Wallet extends StatelessWidget {
+  final String title;
+
+  const Wallet({required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xffdee4eb),
+      appBar: AppBar(
+        title: Text(
+          title,
+          style: TextStyle(
+            color: Colors.black54,
+            fontWeight: FontWeight.bold,
+            fontSize: 21.0,
+          ),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.white,
+        elevation: 0,
+        leading: IconButton(
+          icon: Icon(
+            Icons.menu,
+            color: Colors.black54,
+          ),
+          onPressed: () {},
+        ),
+        actions: <Widget>[
+          IconButton(
+            icon: Container(
+              height: 21,
+              width: 21,
+              child: Stack(
+                alignment: Alignment.center,
+                children: <Widget>[
+                  Align(
+                    alignment: Alignment.topRight,
+                    child: Container(
+                      width: 10,
+                      height: 10,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: Colors.red,
+                      ),
+                    ),
+                  ),
+                  Icon(
+                    Icons.notifications,
+                    color: Colors.black54,
+                  ),
+                ],
+              ),
+            ),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Container(
+              decoration: BoxDecoration(
+                boxShadow: [
+                  BoxShadow(
+                    blurRadius: 5.0,
+                    color: Colors.grey.shade300,
+                    spreadRadius: 5.0,
+                  ),
+                ],
+                borderRadius: BorderRadius.only(
+                  bottomRight: Radius.circular(51),
+                  bottomLeft: Radius.circular(51),
+                ),
+                color: Colors.white,
+              ),
+              child: GestureDetector(
+                child: CreditCardContainer(),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (_) {
+                        return MyCards(title: "My Cards");
+                      },
+                    ),
+                  );
+                },
+              ),
+            ),
+            CustomContainer(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  CustomIconButton(
+                    circleColor: IconColors.send,
+                    buttonImg: "assets/imgs/send.png",
+                    buttonTitle: "SEND",
+                    onTap: () {},
+                  ),
+                  CustomIconButton(
+                    circleColor: IconColors.transfer,
+                    buttonImg: "assets/imgs/transfer.png",
+                    buttonTitle: "TRANSFER",
+                    onTap: () {},
+                  ),
+                  CustomIconButton(
+                    circleColor: IconColors.passbook,
+                    buttonImg: "assets/imgs/passbook.png",
+                    buttonTitle: "PASSBOOK",
+                    onTap: () {},
+                  ),
+                  CustomIconButton(
+                    circleColor: IconColors.more,
+                    buttonImg: "assets/imgs/more.png",
+                    buttonTitle: "MORE",
+                    onTap: () {},
+                  ),
+                ],
+              ),
+            ),
+            CustomContainer(
+              child: Column(
+                children: <Widget>[
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Text(
+                        "Transactions",
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      CustomRoundedButton(
+                        buttonText: "More",
+                        color: Colors.blue,
+                        onTap: () {},
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 15.0,
+                  ),
+                  ListView(
+                    shrinkWrap: true,
+                    scrollDirection: Axis.vertical,
+                    children: <Widget>[
+                      HistoryListTile(
+                        iconColor: IconColors.transfer,
+                        onTap: () {},
+                        transactionAmount: "+\$210.00",
+                        transactionIcon: IconImgs.transfer,
+                        transactionName: "Zohaib Khan",
+                        transactionType: "TRANSFER",
+                      ),
+                      HistoryListTile(
+                        iconColor: IconColors.transfer,
+                        onTap: () {},
+                        transactionAmount: "-\$310.00",
+                        transactionIcon: IconImgs.transfer,
+                        transactionName: "Gulpiya Jaan",
+                        transactionType: "TRANSFER",
+                      ),
+                      HistoryListTile(
+                        iconColor: IconColors.send,
+                        onTap: () {},
+                        transactionAmount: "-\$210.00",
+                        transactionIcon: IconImgs.send,
+                        transactionName: "Wife",
+                        transactionType: "SEND",
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class HistoryListTile extends StatelessWidget {
+  final Color iconColor;
+  final String transactionName,
+      transactionType,
+      transactionAmount,
+      transactionIcon;
+  final GestureTapCallback onTap;
+
+  const HistoryListTile({
+    required this.iconColor,
+    required this.transactionName,
+    required this.transactionType,
+    required this.transactionAmount,
+    required this.transactionIcon,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: Colors.transparent,
+      child: ListTile(
+        title: Text(transactionName),
+        subtitle: Text(transactionType),
+        trailing: Text(transactionAmount),
+        leading: CircleAvatar(
+          radius: 25,
+          child: Image.asset(
+            transactionIcon,
+            height: 25,
+            width: 25,
+          ),
+          backgroundColor: iconColor,
+        ),
+        enabled: true,
+        onTap: onTap,
+      ),
+    );
+  }
+}
+
+class CustomRoundedButton extends StatelessWidget {
+  final Color color;
+  final String buttonText;
+  final GestureTapCallback onTap;
+
+  const CustomRoundedButton({
+    required this.color,
+    required this.buttonText,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: InkWell(
+        onTap: onTap,
+        child: Container(
+          padding: EdgeInsets.symmetric(horizontal: 13.0, vertical: 7.0),
+          decoration: BoxDecoration(
+            color: Colors.transparent,
+            border: Border.all(color: color),
+            borderRadius: BorderRadius.circular(15.0),
+          ),
+          child: Text(
+            "More",
+            style: TextStyle(color: color),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class CustomIconButton extends StatelessWidget {
+  final String buttonTitle, buttonImg;
+  final GestureTapCallback onTap;
+  final Color circleColor;
+
+  const CustomIconButton({
+    required this.buttonTitle,
+    required this.buttonImg,
+    required this.onTap,
+    required this.circleColor,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: Colors.transparent,
+      child: InkWell(
+        onTap: onTap,
+        child: Container(
+          padding: EdgeInsets.all(5.0),
+          alignment: Alignment.center,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                radius: 20,
+                backgroundColor: circleColor,
+                child: Image.asset(
+                  buttonImg,
+                  height: 19,
+                  width: 19,
+                ),
+              ),
+              SizedBox(
+                height: 5.0,
+              ),
+              Text(
+                buttonTitle,
+                overflow: TextOverflow.clip,
+                style: TextStyle(),
+                textAlign: TextAlign.center,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class CreditCardContainer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 31, vertical: 21),
+      decoration: BoxDecoration(
+        boxShadow: [
+          BoxShadow(
+            blurRadius: 5.0,
+            color: Colors.red.shade200,
+            offset: Offset(0, 5),
+          ),
+        ],
+        borderRadius: BorderRadius.circular(15.0),
+        gradient: LinearGradient(
+          colors: [
+            Color(0xffff8964),
+            Color(0xffff5d6e),
+          ],
+        ),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(13.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Container(
+              child: Image.asset(
+                "assets/imgs/chip.png",
+                width: 51,
+                height: 51,
+              ),
+            ),
+            Text(
+              "4000 1234 5678 9010",
+              style: TextStyle(color: Colors.white, fontSize: 25),
+            ),
+            SizedBox(
+              height: 11,
+            ),
+            Row(
+              children: <Widget>[
+                Column(
+                  children: <Widget>[
+                    Text(
+                      "VALID FROM: ",
+                      style: TextStyle(color: Colors.white, fontSize: 11.0),
+                    ),
+                    Text(
+                      "01/24",
+                      style: TextStyle(color: Colors.white, fontSize: 17),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  width: 21,
+                ),
+                Column(
+                  children: <Widget>[
+                    Text(
+                      "VALID THRU: ",
+                      style: TextStyle(color: Colors.white, fontSize: 11.0),
+                    ),
+                    Text(
+                      "01/29",
+                      style: TextStyle(color: Colors.white, fontSize: 17),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 11,
+            ),
+            Text(
+              "Zohaib Khan",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 19,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class CustomContainer extends StatelessWidget {
+  final Widget child;
+
+  const CustomContainer({Key? key, required this.child}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(15.0),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.3),
+            spreadRadius: 3,
+            blurRadius: 7,
+            offset: Offset(0, 3),
+          ),
+        ],
+      ),
+      child: Stack(
+        children: [
+          // Add animated background here
+          AnimatedContainer(
+            duration: Duration(seconds: 1),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              gradient: LinearGradient(
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                colors: [Colors.blue.shade100, Colors.blue.withOpacity(0)],
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 21),
+            child: child,
+          ),
+        ],
+      ),
+    );
+  }
+}
